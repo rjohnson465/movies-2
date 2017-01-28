93e86402aa362c27e1fe51b4c39b4783acd2eac8
Ryan Johnson <rjohnson465@gmail.com>
2017-01-28 17:06:19 -0500

    second commit

--- movie_data.rb
+++ movie_data.rb
@@ -1,4 +1,5 @@
 require 'byebug'
+require 'set'
 class MovieData
 
   attr_accessor :test_set_users, :test_set_movies
@@ -111,14 +112,12 @@ class MovieData
     end
 
     return MovieTest.new(arr)
-
-
   end
 
   # returns a floating point number between 1.0 and 5.0 as an estimate of what user u would give user m
   def predict(u, m)
     # check if user u has seen m; if so, just return their exact rating of m
-    self.train_set_users[u].each do |hash|
+    train_set_users[u].each do |hash|
       if (hash["movie_id"] == m)
         return hash["rating"]
       end
@@ -129,35 +128,43 @@ class MovieData
     # Find all users who have seen m
     hashes_arr = train_set_movies[m]
     seen_m = Hash.new()
-    hashes_arr.each do |hash|
-      seen_m[hash["user_id"]] = hash["rating"]
+    if hashes_arr != nil
+      hashes_arr.each do |hash|
+        seen_m[hash["user_id"]] = hash["rating"]
+      end
+    else
+      return 4 # if no one has seen m in the train set, just guess 4
     end
 
-    # For every user s who has seen m, multiply their rating by their weight (similarity percentage)
+    # For every user s who has seen m, multiply their rating by their weight (similarity percentage; i.e. 50% -> 50)
     adjusted_ratings = 0 # sum of all adjusted ratings
     total_weight = 0 # sum of all weights
     seen_m.each do |user, rating|
-      weight = similarity_hash[user]
-      total_weight += weight
-      adjusted_rating = rating * weight
-      adjusted_ratings += adjusted_rating
+      if (similarity_hash[user] != nil)
+        weight = similarity_hash[user]
+        total_weight += weight
+        adjusted_rating = rating * weight
+        adjusted_ratings += adjusted_rating
+      end
     end
-
     # Divide total adjusted ratings over total weight for prediction
+    if (total_weight != 0)
     prediction = adjusted_ratings / total_weight
+    else
+      prediction = 4 # if we're about to divde by 0, just guess 4
+    end
     return prediction
-
   end
 
   # return a percentage which indicates the similarity in movie preference between user1/user2
   def similarity(user1, user2)
     similarity = 0
+    bt = Time.now
     # iterate over each movie of user with smaller data set
     # if user with larger data set has movie, check if user1's rating == user2's rating (+ or - 1)
     smaller_user = (train_set_users[user1].length < train_set_users[user2].length) ? user1 : user2
     larger_user = (smaller_user == user1) ? user2 : user1
     train_set_users[smaller_user].each do |h1|
-
       movie_id_smaller = h1["movie_id"]
       movie_rating_smaller = h1["rating"]
 
@@ -165,34 +172,82 @@ class MovieData
 
         movie_id_larger = h2["movie_id"]
         movie_rating_larger = h2["rating"]
-        similar_ratings = movie_rating_larger == movie_rating_smaller || movie_rating_larger.to_f == movie_rating_smaller.to_f - 1 || movie_rating_larger.to_f == movie_rating_smaller.to_f + 1
 
-        # if both users have seen the same movie and have similar ratings for that movie, increment similarity
-        if movie_id_smaller == movie_id_larger && similar_ratings
-          similarity += 1
+        # if both users have seen the same movie, increase similarirty based on the differential between user1 and user2's ratings
+        if movie_id_smaller == movie_id_larger #&& similar_ratings
+          rating_diff = (movie_rating_larger - movie_rating_smaller).abs
+          sim_quotient = (movie_rating_larger - rating_diff).abs / 4 # 4 is the max differential (5-1 = 4)
+          similarity += sim_quotient
         end
       end
     end
 
+
     # get total number of movies for the user with the smaller data set
     smaller_user_movies_watched = train_set_users[smaller_user].length.to_f
-
     # divide raw similarity score over total number of movies for the user with the smaller data set and * by 100 to get a percentage
+    et = Time.now
+    #puts "Similarity calc for user #{user1} and user #{user2} took #{(et - bt)*1000} ms to run"
     return (similarity / smaller_user_movies_watched) * 100
   end
 
   # return hash (not a list, for the purpose of preserving similarity percentages) of users whose tastes are most similar to tastes of user u
+  # TODO THIS IS SLOW AF -- why? We are checking the similarity of u and EVERY SINGLE OTHER user -- that's 943 (for dataset u1) runs of similarity()
   def most_similar(u)
     similarity_hash = Hash.new()
+    beginning_time = Time.now
+
+=begin
+    # METHOD TWO get similarity scores only for u and top 100 users who have seen many movies that u has seen
+    bt2 = Time.now
+    # make hash sim_users s.t. key = user s, value = # of movies user s has seen that user u has seen
+    movies_u = movies(u)
+    sim_users = Hash.new()
+    movies_u.each do |movie|
+      viewers(movie).each do |user|
+        if (sim_users[user] == nil)
+          sim_users[user] = 0
+        end
+        sim_users[user] += 1
+      end
+    end
+
+    # use 100 users from sim_hash with the highest values ((movies in common with u)
+    sim_users = sim_users.sort_by {|k, v| v}.reverse.to_h
+    length = (sim_users.length < 25) ? sim_users.length : 25
+    sim_users = sim_users.first(length).to_h
+
+    sim_users.each do |user, val|
+      otherUser = user
+      if otherUser != u
+        similarity = similarity(u, otherUser)
+        if (similarity >= 0)
+          similarity_hash[otherUser] = similarity
+        end
+      end
+    end
+=end #end METHOD TWO
+
+    # METHOD ONE get similarity scores for u and every other member of the dataset (very slow, ~1200 ms per prediction)
+    bt2 = Time.now
+    # TODO reduce number of similarity calculations (ideally to 100 calculations / prediction)
     train_set_users.each do |key, array|
       otherUser = key
       if otherUser != u
         similarity = similarity(u, otherUser)
-        similarity_hash[otherUser] = similarity
+        if (similarity >= 0)
+          similarity_hash[otherUser] = similarity
+        end
       end
-    end
+    end # end METHOD ONE
+    et2 = Time.now
+    puts "Hashing all similarities for user #{u} took #{(et2 - bt2)*1000} ms to run"
+    end_time = Time.now
+    puts "Most similar function took #{(end_time - beginning_time)*1000} milliseconds to run"
+
     return similarity_hash.sort_by {|k, v| v}.reverse.to_h
   end
+
 end # end MovieData class
 
 
@@ -208,8 +263,8 @@ class MovieTest
   def mean()
     total_diff = 0
     data.each do |arr|
-      predicted = arr[3]
-      actual = arr[2]
+      predicted = arr[3].to_f
+      actual = arr[2].to_f
       differential = (predicted - actual).abs
       total_diff += differential
     end
@@ -225,8 +280,8 @@ class MovieTest
   def rms()
     sum_diff_squares = 0
     data.each do |arr|
-      predicted = arr[3]
-      actual = arr[2]
+      predicted = arr[3].to_f
+      actual = arr[2].to_f
       diff_squared = (actual - predicted) ** 2
       sum_diff_squares += diff_squared
     end
@@ -241,10 +296,11 @@ class MovieTest
 
 end
 
-sample_size = 3
+sample_size = 100
 md = MovieData.new(:u1)
 mt = md.run_test(sample_size)
+
 puts "Sample Size: #{sample_size}"
-puts "Mean Error: " + mt.mean.to_s
-puts "Standard Deviation: " + mt.stddev.to_s
-puts "Root Mean Square Error: " + mt.rms.to_s
+puts "Mean Error: #{mt.mean}"
+puts "Standard Deviation: #{mt.stddev}"
+puts "Root Mean Square Error: #{mt.rms}"
